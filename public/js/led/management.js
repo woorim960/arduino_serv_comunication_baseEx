70722edf62363e366845ba681450a4fa8aad073f
박우림 <woorimprog@gmail.com>
2020-07-13 21:04:10 +0900

    주석 추가

--- public/js/led/management.js
+++ public/js/led/management.js
@@ -1,12 +1,16 @@
 /* public/js/led/management.js */
 'use strict';
 
+// 버튼 변수 선언
 const ledOnBtn = document.querySelector('#led-on'),
   ledOffBtn = document.querySelector('#led-off');
 
+// LED 상태 메세지 변수 선언
 const status = document.querySelector('#status');
 
+// LED On 함수
 function onLed() {
+  // fetch()로 post 메소드 요청
   fetch('/on', {
     method : 'POST',
     headers : {
@@ -15,13 +19,16 @@ function onLed() {
   })
     .then((promise) => promise)
     .then((result) => {
+      // 반환된 결과가 true이면 실행
       if (result) {
         status.innerText = `LED가 켜졌습니다`;
       }
     });
 }
 
+// LED Off 함수
 function offLed() {
+  // fetch()로 post 메소드 요청
   fetch('/off', {
     method : 'POST',
     headers : {
@@ -30,15 +37,19 @@ function offLed() {
   })
   .then((promise) => promise)
   .then((result) => {
+    // 반환된 결과가 true이면 실행
     if (result) {
       status.innerText = `LED가 꺼졌습니다`;
     }
   });
 }
 
+// 초기 실행 함수
 function init() {
+  // 버튼 이벤드
   ledOnBtn.addEventListener('click', onLed);
   ledOffBtn.addEventListener('click', offLed);
 }
 
+// 함수 실행
 init();
--- routes/led.js
+++ routes/led.js
@@ -10,23 +10,33 @@ router.get('/', (req, res) => {
   res.render('led/management');
 });
 
+// /on으로 post 메소드로 요청이 올 경우
 router.post('/on', (req, res) => {
+  // 시리얼 포트를 사용하기 위해 오픈
   serialPort.open(() => {
 
+    // 해당 포트로 1 전달 (LED 켜짐)
     serialPort.write('1', (err, results) => {
-      // LED 켜짐
+      // 에러 처리
       if (err) throw err;
+
+      // /on으로 post 메소드를 요청한 fetch()에게 true 응답
       res.json(true);
     });
   });
 });
 
+// /off으로 post 메소드로 요청이 올 경우
 router.post('/off', (req, res) => {
+  // 시리얼 포트를 사용하기 위해 오픈
   serialPort.open(() => {
 
+    // 해당 포트로 1 전달 (LED 꺼짐)
     serialPort.write('0', (err, results) => {
-      // LED 꺼짐
+      // 에러 처리
       if (err) throw err;
+
+      // /off으로 post 메소드를 요청한 fetch()에게 true 응답
       res.json(true);
     });
   });
